007cf876cbd34f841ad229c6e0925e067fd9a618
Mickael Pham <mickael.pham@gmail.com>
2017-08-27 21:56:36 -0700

    Refactor ship spec using shared examples

--- spec/battleship/ship_spec.rb
+++ spec/battleship/ship_spec.rb
@@ -1,19 +1,26 @@
 # frozen_string_literal: true
 
-RSpec.describe Battleship::Ship do
-  let(:size)   { 5 }
-  let(:ship) { described_class.new(size) }
+RSpec.shared_examples 'a ship' do
+  let(:ship) { described_class.new }
 
   describe '#size' do
     subject { ship.size }
-    it { is_expected.to eq(size) }
+    it { is_expected.to eq(expected_size) }
   end
 
   describe '#hit' do
     it 'removes 1 HP' do
-      previous = ship.hit_points
-      ship.hit
-      expect(ship.hit_points).to eq(previous - 1)
+      expect { ship.hit }.to change(ship, :hit_points).by(-1)
+    end
+  end
+
+  describe '#hit_points' do
+    subject { ship.hit_points }
+    it { is_expected.to eq(ship.size) }
+
+    context 'until destroyed' do
+      before { ship.size.times { ship.hit } }
+      it { is_expected.to eq(0) }
     end
   end
 
@@ -27,8 +34,43 @@ RSpec.describe Battleship::Ship do
     end
 
     context 'after enough hits' do
-      before { size.times { ship.hit } }
+      before { ship.size.times { ship.hit } }
       it { is_expected.to eq(true) }
     end
   end
+
+  describe '#name' do
+    subject { ship.name }
+    it { is_expected.to eq(expected_name) }
+  end
+end
+
+RSpec.describe Battleship::Ship::Destroyer do
+  let(:expected_size) { 2 }
+  let(:expected_name) { 'Destroyer' }
+  it_behaves_like 'a ship'
+end
+
+RSpec.describe Battleship::Ship::Submarine do
+  let(:expected_size) { 3 }
+  let(:expected_name) { 'Submarine' }
+  it_behaves_like 'a ship'
+end
+
+RSpec.describe Battleship::Ship::Cruiser do
+  let(:expected_size) { 3 }
+  let(:expected_name) { 'Cruiser' }
+  it_behaves_like 'a ship'
+end
+
+RSpec.describe Battleship::Ship::Warship do
+  let(:expected_size) { 4 }
+  let(:expected_name) { 'Warship' }
+  it_behaves_like 'a ship'
+end
+
+RSpec.describe Battleship::Ship::Carrier do
+  let(:expected_size) { 5 }
+  let(:expected_name) { 'Carrier' }
+  it_behaves_like 'a ship'
 end
